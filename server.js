e4dca022c329ef92d10fa5ef0307cdf5a418a8fc
Liam Swinney <liam.swinney@bedegaming.com>
2016-12-01 15:06:50 +0000

    use the correct server

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const express = require('express');
 const WSS = require('ws').Server;
 const app = express().use(express.static('public'));
 const port = process.env.PORT || 8080;
+const server = http.createServer(app);
 
-var app = express().use(express.static('public'));
-var server = http.createServer(app);
 server.listen(port);
 
-var wss = new WSS({server: server});
+const wss = new WSS({server});
+
 wss.on('connection', socket => {
   const json = JSON.stringify({ message: 'Server connected' });
   socket.send(json);
